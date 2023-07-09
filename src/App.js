c6db24c7e30afaab8180fa9a03ac9a13f2f9dd64
debojitsaha <saha05debojit@gmail.com>
2023-07-09 10:49:17 +0000

    Working on contact page

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import AppState from "./context-api/AppState";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import HomePage from './pages/Homepage'
-import Profile from './pages/Profile'
+import HomePage from "./pages/Homepage";
+import Profile from "./pages/Profile";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <AppState>
         <ChakraProvider>
-        <BrowserRouter>
-        <Navbar/>
-        <Routes>
-        <Route index path="" element={<HomePage />} />
-        <Route index path="profile" element={<Profile />} />  
-        </Routes>
-        <Footer/>
-        </BrowserRouter>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route index path="" element={<HomePage />} />
+              <Route index path="profile" element={<Profile />} />
+              <Route index path="contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
         </ChakraProvider>
       </AppState>
     </>
