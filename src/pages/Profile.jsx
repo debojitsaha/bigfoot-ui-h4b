049ada1d0a9c4ace74470964629ce7d3c311982f
debojitsaha <saha05debojit@gmail.com>
2023-07-10 07:09:35 +0000

    Fixed: Profile Logout Loading

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,44 +1,65 @@
-import React, { useContext } from "react";
-import { Avatar, Box, HStack, Spinner, Tag, TagLabel, TagLeftIcon, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import React, { useContext, useEffect } from "react";
+import {
+  Avatar,
+  Box,
+  HStack,
+  Spinner,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  Text,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
 import { BiPhoneCall } from "react-icons/bi";
 import { AiOutlineMail } from "react-icons/ai";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { MdOutlineAttachMoney, MdOutlineDoneAll } from "react-icons/md";
 import appContext from "../context-api/appContext";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Profile = () => {
   const { userProfile } = useContext(appContext);
   // console.log(userProfile);
+  const location = useLocation();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!userProfile) {
+      navigate("/");
+    }
+  }, [location, userProfile]);
 
   return (
     <Box p={6} display={"flex"} w={"100vw"} justifyContent={"space-evenly"}>
-      {userProfile ? (
-        <Box
-          p={16}
-          borderRadius={12}
-          boxShadow={"0px 0px 7px 0px rgba(0, 0, 0, 0.25)"}
-          display={"flex"}
-          flexDirection={"column"}
-          gap={5}
-        >
-          <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-            <Avatar
-              name={userProfile.name}
-              size="xl"
-              src={userProfile.image}
-              alt={userProfile.name}
-              mb={3}
-            />
-            <Text color={"#0EA293"} fontSize={"24px"} fontWeight={500}>
-              {userProfile.name}
-            </Text>
-            <Text fontSize={"16px"} fontWeight={500}>
-              {userProfile.dob}
-            </Text>
-            <Text fontSize={"16px"} fontWeight={500}>
-              {userProfile.sex[0].toUpperCase()}, {userProfile.age}
-            </Text>
-          </Box>
+      <Box
+        p={16}
+        borderRadius={12}
+        boxShadow={"0px 0px 7px 0px rgba(0, 0, 0, 0.25)"}
+        display={"flex"}
+        flexDirection={"column"}
+        gap={5}
+      >
+        <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+          <Avatar
+            name={userProfile ? userProfile.name : "Login"}
+            size="xl"
+            src={userProfile ? userProfile.image : "No"}
+            alt={userProfile ? userProfile.name : "No"}
+            mb={3}
+          />
+          <Text color={"#0EA293"} fontSize={"24px"} fontWeight={500}>
+            {userProfile ? userProfile.name : ""}
+          </Text>
+          <Text fontSize={"16px"} fontWeight={500}>
+            {userProfile ? userProfile.dob : ""}
+          </Text>
+          <Text fontSize={"16px"} fontWeight={500}>
+            {userProfile ? userProfile.sex[0].toUpperCase() : ""},{" "}
+            {userProfile ? userProfile.age : ""}
+          </Text>
+        </Box>
+        {userProfile && (
           <Box display={"flex"} flexDirection={"column"}>
             <Text fontSize={"18px"} fontWeight={500}>
               Contact
@@ -46,46 +67,38 @@ const Profile = () => {
             <Box display={"flex"} alignItems={"center"} gap={2}>
               <AiOutlineMail size={"16px"} />
               <Text fontSize={"16px"} fontWeight={500}>
-                {userProfile.email}
+                {userProfile ? userProfile.email : ""}
               </Text>
             </Box>
             <Box display={"flex"} alignItems={"center"} gap={2}>
               <BiPhoneCall size={"16px"} />
               <Text fontSize={"16px"} fontWeight={500}>
-                {userProfile.phone}
+                {userProfile ? userProfile.phone : ""}
               </Text>
             </Box>
             <Box display={"flex"} alignItems={"center"} gap={2}>
               <HiOutlineLocationMarker size={"16px"} />
               <Text fontSize={"16px"} fontWeight={500}>
-                {userProfile.country}
+                {userProfile ? userProfile.country : ""}
               </Text>
             </Box>
           </Box>
-        </Box>
-      ) : (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      )}
+        )}
+      </Box>
       <Box>
         <Box display={"flex"} justifyContent={"space-between"}>
           <Text color={"#0EA293"} fontSize={"28px"} fontWeight={500}>
             Earned Badges
           </Text>
           <HStack spacing={1}>
-          <Tag size={"md"} variant="subtle" colorScheme="cyan">
-            <TagLeftIcon as={MdOutlineAttachMoney} />
-            <TagLabel>50</TagLabel>
-          </Tag>
-          <Tag size={"md"} variant="solid" colorScheme="yellow">
-            <TagLeftIcon as={MdOutlineDoneAll} />
-            <TagLabel>6</TagLabel>
-          </Tag>
+            <Tag size={"md"} variant="subtle" colorScheme="cyan">
+              <TagLeftIcon as={MdOutlineAttachMoney} />
+              <TagLabel>50</TagLabel>
+            </Tag>
+            <Tag size={"md"} variant="solid" colorScheme="yellow">
+              <TagLeftIcon as={MdOutlineDoneAll} />
+              <TagLabel>6</TagLabel>
+            </Tag>
           </HStack>
         </Box>
         <Wrap mt={2}>
